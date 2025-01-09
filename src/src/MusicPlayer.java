1d905d0a3ff006fb8acaa2afec0803cb1195b40e
Rakesh Gupta <rakeshgupta1361411@gmail.com>
2025-01-09 13:30:25 +0530

    doen

--- src/src/MusicPlayer.java
+++ src/src/MusicPlayer.java
@@ -3,14 +3,12 @@ package src;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-
-
-
-
 
 import java.io.File;
 
@@ -18,16 +16,51 @@ public class MusicPlayer extends Application {
 
     private MediaPlayer mediaPlayer;
     private String[] songs = {
-    	"C:\\Users\\Rakesh\\eclipse-workspace\\Music\\src\\musiclist\\song1.mp3",
-        "C:\\Users\\Rakesh\\eclipse-workspace\\Music\\src\\musiclist",
+        "C:\\Users\\Rakesh\\eclipse-workspace\\Music\\src\\musiclist\\song1.mp3",
+        "C:\\Users\\Rakesh\\eclipse-workspace\\Music\\src\\musiclist\\song2.mp3",
         "C:\\Users\\Rakesh\\eclipse-workspace\\Music\\src\\musiclist\\song3.mp3"
     };
     private int currentSongIndex = 0;
 
     @Override
     public void start(Stage primaryStage) {
+        showSongSelectionMenu(primaryStage);
+    }
+
+    private void showSongSelectionMenu(Stage primaryStage) {
+        // Create a ListView to display the songs
+        ListView<String> songListView = new ListView<>();
+        for (String song : songs) {
+            songListView.getItems().add(new File(song).getName());
+        }
+
+        // Button to proceed to the player
+        Button selectButton = new Button("Select and Play");
+        selectButton.setOnAction(e -> {
+            int selectedSongIndex = songListView.getSelectionModel().getSelectedIndex();
+            if (selectedSongIndex != -1) {
+                currentSongIndex = selectedSongIndex;
+                showPlayerPanel(primaryStage);
+            } else {
+                System.out.println("Please select a song first.");
+            }
+        });
+
+        // Layout for the menu
+        BorderPane menuLayout = new BorderPane();
+        menuLayout.setCenter(songListView);
+        menuLayout.setBottom(selectButton);
+
+        Scene menuScene = new Scene(menuLayout, 400, 300);
+        primaryStage.setTitle("Select a Song");
+        primaryStage.setScene(menuScene);
+        primaryStage.show();
+    }
+
+    private void showPlayerPanel(Stage primaryStage) {
         loadSong(currentSongIndex);
 
+        // Create player controls
         Button playButton = new Button("Play");
         playButton.setOnAction(e -> mediaPlayer.play());
 
@@ -51,11 +84,20 @@ public class MusicPlayer extends Application {
             mediaPlayer.play();
         });
 
-        HBox controls = new HBox(10, playButton, pauseButton, stopButton, previousButton, nextButton);
-        Scene scene = new Scene(controls, 400, 100);
+        // Add a Back to Menu button
+        Button backButton = new Button("Back to Menu");
+        backButton.setOnAction(e -> {
+            if (mediaPlayer != null) {
+                mediaPlayer.stop();
+            }
+            showSongSelectionMenu(primaryStage);
+        });
+
+        HBox controls = new HBox(15, playButton, pauseButton, stopButton, previousButton, nextButton, backButton);
+        Scene playerScene = new Scene(controls, 400, 100);
 
         primaryStage.setTitle("Music Player");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(playerScene);
         primaryStage.show();
     }
 
